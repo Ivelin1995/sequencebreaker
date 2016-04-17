c951b88da000bff468e753550c8d969011e8812f
Ivelin <ivelinivanov1995@gmail.com>
2016-04-17 00:17:12 -0700

    Points added, +5 for pass -2 for fail, feel free to change them

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
+session_start();
+
+if(!isset($_SESSION['points'])){
+	$_SESSION['points']=0;
+}
 
 if(!empty($_POST['input'])){
 	if($_POST['input']==$_POST['answer']){
 		echo "PASSED";
+		$_SESSION['points']+=5;
 	}else{
 		echo "FAILED";
+		$_SESSION['points']-=2;
 	}
 }
+
+
 
 $arr = array(
 array(1,2,3,4),
@@ -41,12 +50,15 @@ for($i=0;$i<count($selectedArr)-1;$i++){
 ?>
 <div>
 <div id="center2">
-<form action="" method="post">
-  <input type="text" name="input"><br>
-  <input type="hidden" name="answer" value=<?php 
-echo '"'.$_POST['answer']=$selectedArr[count($selectedArr)-1].'"';?>>
-  <input type="submit" value="Submit">
-</form>
+	<form action="" method="post">
+	  <input type="text" name="input"><br>
+	  <input type="hidden" name="answer" value=<?php 
+		echo '"'.$_POST['answer']=$selectedArr[count($selectedArr)-1].'"';?>>
+	  <input type="submit" value="Submit">
+	</form>
 <div>
+<div id="topright">
+	Points: <?php echo $_SESSION['points']; ?>
+</div>
 <body>
 <html>
